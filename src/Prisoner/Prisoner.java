ceecf0d765a32234c0cb3e6bd84b41d5d4a442d2
WX-kfc <923071808@qq.com>
2015-11-06 21:30:08 +0800

    Initial commit

---
+++ src/Prisoner/Prisoner.java
@@ -0,0 +1,7 @@
+package Prisoner;
+
+/**
+ * Created by MSI on 2015/11/6.
+ */
+public interface Prisoner {
+}
---
+++ src/douzi.java
@@ -0,0 +1,42 @@
+
+/**
+ * Created by MSI on 2015/11/6.
+ */
+    package com.jude.prisoner;
+     import com.jude.Manger;
+
+    public class SimilarMangermode implements Prisoner {
+        int totalCount;
+        int totalPerson;
+
+        @Override
+        public String getName() {
+            return "Œ‚‰Ï2015213186";
+        }
+
+        @Override
+        public void begin(Manager manager,int totalPerson, int totalCount) {
+            this.totalCount = totalCount;
+            this.totalPerson = totalPerson;
+        }
+
+        @Override
+        public int take(int index, int last) {
+            int average=totalCount/totalPerson;
+            int lastaverage=(totalCount-last)/index;
+            //À≠ª·À¿ƒÿ?
+            if(index==0)
+            {
+                return average;
+            }
+            else if(index==1)
+                return (totalCount-last-1);
+            else
+                return (totalCount -last)/index;
+        }
+
+        @Override
+        public void result(boolean survived) {
+        }
+    }
+}
